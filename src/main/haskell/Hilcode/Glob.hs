f42b7994674a51ccffc1ab1a221624d02f24f7b6
Hilco Wijbenga <hilco.wijbenga+hilcode@gmail.com>
2025-10-21 13:58:48 -0700

    10

--- src/main/haskell/Hilcode/Glob.hs
+++ src/main/haskell/Hilcode/Glob.hs
@@ -2,7 +2,12 @@ module Hilcode.Glob (
     mkGlob,
 ) where
 
+import Control.Monad qualified
 import Data.List (unsnoc)
+import Data.Set (Set)
+import Data.Set qualified
+import Data.Vector (Vector)
+import Data.Vector qualified
 import Hilcode.Result (
     Result,
     err,
@@ -10,6 +15,7 @@ import Hilcode.Result (
     mapError,
     ok,
  )
+import System.Directory.OsPath qualified
 import System.IO (
     utf16,
     utf8,
@@ -27,27 +33,170 @@ import System.OsPath (
     unsafeEncodeUtf,
     unsafeFromChar,
  )
+import System.OsString qualified
 
 data GlobPart
     = AnyChars
     | AnySingleChar
     | Verbatim OsString
-    deriving stock (Show)
+    deriving stock (Eq, Ord, Show)
+
+data GlobPartFiber
+    = GlobPartFiber OsString [GlobPart]
+    deriving stock (Eq, Ord)
+
+_matchGlobPartFiber :: GlobPartFiber -> Either (Set GlobPartFiber) Bool
+_matchGlobPartFiber (GlobPartFiber name []) =
+    Right $ System.OsString.null name
+_matchGlobPartFiber (GlobPartFiber name (Verbatim prefix : globParts)) =
+    case System.OsString.stripPrefix prefix name of
+        Nothing ->
+            Right False
+        Just tailOsChars ->
+            Left $ Data.Set.singleton (GlobPartFiber tailOsChars globParts)
+_matchGlobPartFiber (GlobPartFiber name (AnySingleChar : globParts)) =
+    case System.OsString.uncons name of
+        Nothing ->
+            Right False
+        Just (_, tailOsChars) ->
+            Left $ Data.Set.singleton (GlobPartFiber tailOsChars globParts)
+_matchGlobPartFiber (GlobPartFiber name allGlobParts@(AnyChars : globParts)) =
+    case System.OsString.uncons name of
+        Nothing ->
+            Left $ Data.Set.singleton (GlobPartFiber name globParts)
+        Just (_, tailOsChars) ->
+            let
+                skipAnyChars = GlobPartFiber name globParts
+                matchAnyCharsOnce = GlobPartFiber tailOsChars globParts
+                matchAnyCharsAgain = GlobPartFiber tailOsChars allGlobParts
+             in
+                Left $ Data.Set.fromList [skipAnyChars, matchAnyCharsOnce, matchAnyCharsAgain]
 
 data DirGlob
     = AnyDirs
     | AnySingleDir
     | DirMatch [GlobPart]
-    deriving stock (Show)
+    deriving stock (Eq, Ord, Show)
+
+_matchSingle :: OsString -> DirGlob -> ()
+_matchSingle _singleDirectory AnyDirs =
+    undefined
+_matchSingle _singleDirectory AnySingleDir =
+    undefined
+_matchSingle _singleDirectory (DirMatch _globParts) =
+    undefined
 
 data FileGlob
     = AnySingleFile
     | FileMatch [GlobPart]
-    deriving stock (Show)
+    deriving stock (Eq, Ord, Show)
+
+_matchFileGlob :: OsString -> FileGlob -> Bool
+_matchFileGlob _ AnySingleFile =
+    True
+_matchFileGlob name (FileMatch globParts) =
+    let
+        globPartFiber :: GlobPartFiber
+        globPartFiber = GlobPartFiber name globParts
+     in
+        go $ Data.Set.singleton globPartFiber
+  where
+    go :: Set GlobPartFiber -> Bool
+    go fibers =
+        case simplify Data.Set.empty (_matchGlobPartFiber `fmap` Data.Set.elems fibers) of
+            Left fibers ->
+                go fibers
+            Right result ->
+                result
+      where
+        simplify :: (Monoid a) => a -> [Either a b] -> Either a b
+        simplify accumulator [] =
+            Left accumulator
+        simplify accumulator (ab : abs) =
+            case ab of
+                Left a ->
+                    simplify (accumulator <> a) abs
+                Right b ->
+                    Right b
 
 data Glob
     = Glob [DirGlob] FileGlob
-    deriving stock (Show)
+    deriving stock (Eq, Ord, Show)
+
+data GlobFiber
+    = GlobFiber OsPath Glob
+    deriving stock (Eq, Ord)
+
+data OsPathType
+    = Directory
+    | File
+    | Other
+
+ifM :: IO Bool -> IO a -> IO a -> IO a
+ifM predicateM ifTrue ifFalse = do
+    predicate <- predicateM
+    if predicate then ifTrue else ifFalse
+
+toOsPathType :: OsPath -> IO OsPathType
+toOsPathType osPath = do
+    ifM
+        (System.Directory.OsPath.doesDirectoryExist osPath)
+        (pure Directory)
+        ( ifM
+            (System.Directory.OsPath.doesFileExist osPath)
+            (pure File)
+            (pure Other)
+        )
+
+split :: [OsPath] -> IO ([OsPath], [OsPath])
+split =
+    Control.Monad.foldM step ([], [])
+  where
+    step :: ([OsPath], [OsPath]) -> OsPath -> IO ([OsPath], [OsPath])
+    step (directories, files) osPath = do
+        osPathType <- toOsPathType osPath
+        case osPathType of
+            Directory ->
+                pure (osPath : directories, files)
+            File ->
+                pure (directories, osPath : files)
+            Other ->
+                pure (directories, files)
+
+_matchGlobFiber :: GlobFiber -> IO (Set GlobFiber, Vector OsPath)
+_matchGlobFiber (GlobFiber osPath glob) = do
+    entries <- System.Directory.OsPath.listDirectory osPath
+    (_directories, files) <- split entries
+    case glob of
+        Glob [] fileGlob ->
+            let _x1 = (`_matchFileGlob` fileGlob) `filter` files
+             in undefined
+        Glob (_dirGlob : _dirGlobs) _fileGlob ->
+            undefined
+
+_match :: OsPath -> Glob -> IO (Vector OsPath)
+_match directory glob =
+    let
+        fiber :: GlobFiber
+        fiber = GlobFiber directory glob
+
+        go :: (Set GlobFiber, Vector OsPath) -> IO (Vector OsPath)
+        go (fibers, foundSoFar) =
+            if Data.Set.null fibers
+                then pure foundSoFar
+                else
+                    let
+                        globFibers :: [GlobFiber]
+                        globFibers = Data.Set.elems fibers
+
+                        newState :: IO [(Set GlobFiber, Vector OsPath)]
+                        newState = mapM _matchGlobFiber globFibers
+                     in
+                        do
+                            state :: (Set GlobFiber, Vector OsPath) <- mconcat `fmap` newState
+                            go state
+     in
+        go (Data.Set.singleton fiber, Data.Vector.empty)
 
 mkGlob :: String -> Result GlobError Glob
 mkGlob text =
