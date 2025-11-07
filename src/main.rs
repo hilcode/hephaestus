b6e7b5d833b9faee0c7105019dd7d7b4a20af960
Hilco Wijbenga <hilco.wijbenga+hilcode@gmail.com>
2025-11-07 13:37:13 -0800

    6

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@
 
 mod hilcode;
 
+use std::fs::File;
+use std::fs::Metadata;
 use std::path::Path;
 use std::path::PathBuf;
 use std::rc::Rc;
 
 use clap::Parser;
+use memmap3::Mmap;
+use xxhash_rust::xxh3::xxh3_64;
 
 use crate::hilcode::config::app_config::AppConfig;
 use crate::hilcode::config::cli::Cli;
@@ -28,7 +32,11 @@ fn main()
 	let files: FileSet = file_set_glob.search("rust-sources").unwrap();
 	for (index, file) in files.iter().enumerate()
 	{
-		println!("{}: {:?}", index, file);
+		let metadata: Metadata = file.metadata().unwrap();
+		let file: File = File::open(file).unwrap();
+		let mmap: Mmap = unsafe { Mmap::map(&file).unwrap() };
+		let hash: u64 = xxh3_64(&mmap);
+		println!("{}: {:?} / {:?} / {:?}", index, file, &metadata, hash);
 	}
 	println!("Okay");
 }
